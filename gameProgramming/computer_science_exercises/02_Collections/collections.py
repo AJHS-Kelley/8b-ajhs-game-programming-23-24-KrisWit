8c2edb39a1bafd0c54ff8bc87575f11740321a56
KrisWit <142526887+KrisWit@users.noreply.github.com>
2023-10-02 14:08:32 -0400

    v0.1b
    Print list content

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -1,4 +1,4 @@
-# Collections Examples, Kristopher Cooper v0.1a
+# Collections Examples, Kristopher Cooper v0.1b
 
 # LIST -- ORDERED,CHANGEABLE, ALLOWS DUPLICATE VALUES
 breakfastFoods = ["Bacon", "Waffkes", "Pancakes","Ceral", "Milk"]
@@ -8,3 +8,8 @@ breakfastFoods = ["Bacon", "Waffkes", "Pancakes","Ceral", "Milk"]
 # Python Only: index -1 it is the last item on the list
 testScores = [95, 100, 25, 15, 27, 35]
 classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
+
+# Printing Cotents of an List
+print(breakfastFoods)
+print(testScores)
+print(classGPA)
