0c6240cfc9451554ccea8a792c95c09bb76a1cac
KrisWit <142526887+KrisWit@users.noreply.github.com>
2023-10-17 14:48:21 -0400

    Update collection.py

--- gameProgramming/computer_science_exercises/02_Collections/collection.py
+++ gameProgramming/computer_science_exercises/02_Collections/collection.py
@@ -28,3 +28,31 @@
 #     print("You have the correct key! The door unlocks.\n")
 # else:
 #     print("You do not have that key. The door remains locked.\n")
+
+weaponList = [
+    True, # Sword
+    False, # Laser beam
+    False, # Gun that shoots mines
+    True, # Teleporter
+    False, # RPG
+    False, # Air 
+    True # Knife
+]
+
+weaponNum = 0
+while weaponNum < len(weaponList): 
+    if weaponNum == 0 and weaponList[0] == True: 
+        print("You are wielding a golden sword.\n")
+    if weaponNum == 1 and weaponList[1] == True:
+        print("You have a laser beam\n")
+    if weaponNum == 2 and weaponList[2] == True:
+        print("You have a gun that shoots mines\n")
+    if  weaponNum == 3 and weaponList[3] == True: 
+        print("You have a Teleporter.\n")
+    if weaponNum == 4 and weaponList[4] == True: 
+        print("You have a RPG.\n")
+    if weaponNum == 5 and weaponList[5] == True: 
+        print("You Air.\n")
+    if weaponNum == 6 and weaponList[6] == True: 
+        print("You are wielding a shiny knife.\n")
+    weaponNum += 1
