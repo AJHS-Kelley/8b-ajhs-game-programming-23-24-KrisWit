e090d1e0945c6193f7ad5adf56a4cea74e7d1c20
KrisWit <142526887+KrisWit@users.noreply.github.com>
2023-10-31 14:17:41 -0400

    Update hangman.py

--- 04b_hangman/hangman.py
+++ 04b_hangman/hangman.py
@@ -2,3 +2,46 @@
 
 words = 'snake bird fish bat cat rat cow games flow data control man van band spring summer winter fall hang visual people react raise bell king queen apple pat fat chesse'.split()
 print(words)
+# VARIABLE_NAME in ALL-CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE!
+HANGMAN_BOARD = ['''
+      +---+
+          |           
+          |      
+          |
+        ======''','''
+      +---+
+      O   |           
+          |      
+          |
+        ======''','''
+      +---+
+      O   |           
+      |   |      
+          |
+        ======''','''
+      +---+
+      O   |           
+     /|   |      
+          |
+        ======''','''
+      +---+
+      O   |           
+     /|\  |      
+          |
+        ======''','''
+      +---+
+      O   |           
+     /|\  |      
+     /    |
+        ======''','''
+      +---+
+      O   |           
+     /|\  |      
+     / \  |
+        ======''']
+
+i = 0
+while i < len(HANGMAN_BOARD):
+    print (HANGMAN_BOARD[i])
+    i += 1
+    
