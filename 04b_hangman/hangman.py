d6deba0312c9138103844a464e9b1cb5cb0b5d94
KrisWit <142526887+KrisWit@users.noreply.github.com>
2023-10-31 13:58:28 -0400

    Create hangman.py

---
+++ 04b_hangman/hangman.py
@@ -0,0 +1,4 @@
+# Hangman Game by Kristopher Cooper, v0.1
+
+words = 'snake bird fish bat cat rat cow games flow data control man van band spring summer winter fall hang visual people react raise bell king queen apple pat fat chesse'.split()
+print(words)
