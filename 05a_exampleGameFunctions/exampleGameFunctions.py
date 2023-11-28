fd476702cd59adab2ba3f16e8eb6a8b4a07cb0b7
KrisWit <142526887+KrisWit@users.noreply.github.com>
2023-11-28 14:38:22 -0500

    Done

--- 05a_exampleGameFunctions/exampleGameFunctions.py
+++ 05a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,11 +1,11 @@
-# Example Game Functions Project, Kristopher Cooper v0.1
+# Example Game Functions Project, Kristopher Cooper v2
 import random
 
 def play_game():
     choices = ['rock', 'paper', 'scissors']
     computer_choice = random.choice(choices)
 
-    user_choice = input("Enter your choice (rock/paper/si=cissors): ").lower()
+    user_choice = input("Enter your choice (rock/paper/scissors):\n").lower()
 
     if user_choice not in choices:
         print("Invalid choice, Please try again.:")
@@ -20,5 +20,22 @@ def play_game():
             (user_choice == 'paper' and computer_choice == 'rock') or \
             (user_choice == "scissors" and computer_choice == 'paper'):
         print("You win!")
+        global user_score
+        user_score += 1
     else:
-        print()    
+        print("Computer wins")
+        global computer_score
+        computer_score += 1
+
+    print("Score: You", user_score, "- Computer", computer_score)    
+
+    play_again = input("Do you want to play again? (yes/no):\n").lower()
+    if play_again == 'yes' or "y":
+        play_game()
+    else:
+        print("Thanks for playing!")
+# Initialize scores
+user_score = 0
+computer_score = 0
+
+play_game()
