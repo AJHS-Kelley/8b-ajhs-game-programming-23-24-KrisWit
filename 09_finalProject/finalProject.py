16d191502cc3104d5f1b135663ed80e70b394f1c
KrisWit <142526887+KrisWit@users.noreply.github.com>
2024-03-27 14:48:48 -0400

    game

--- 09_finalProject/finalProject.py
+++ 09_finalProject/finalProject.py
@@ -1,1 +1,8 @@
-# Final Project, Kristopher Cooper v0.0
+# Final Project, Kristopher Cooper v0.0c
+import sys, random , pygame
+
+resolution = 0 # 0 = low resolution, 1 = high resolution
+
+screen = pygame.display.set_mode((X, y))
+# CREAT AN IF / ELSE BLOCK TO SET RESOLUTIOM BASED ON THE VARIABLE ABOVE.
+
